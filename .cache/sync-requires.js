b49f3b90744c5a351def08a2c051db1ba7d549d4
Igor Yermak <igor.yermak@gmail.com>
2018-08-15 23:14:23 -0400

    gatsby 2.0 beta

---
+++ .cache/sync-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/igor/Desktop/lambda/hera/.cache/dev-404-page.js"))
+}
+
